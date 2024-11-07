d9fab43fbc3498fd53b984e0fa29e00cfdacef63
keitake123 <keitaketsuna@gmail.com>
2024-11-06 23:48:32 -0500

    b

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,16 +1,44 @@
-import React from "react";
+// src/App.js
+import React, { useState } from "react";
 import "./App.css";
-
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-   return (
-       <div className = "App">
-           <header className = "App-header">
-               <p>React Todo</p>
-           </header>
-       </div>
-   );
-}
+  const [todos, setTodos] = useState([]);
 
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} />
+      </header>
+    </div>
+  );
+}
 
 export default App;
